7eb89f6d637904a2432650d35820ef6b089fd183
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-04-25 13:53:46 +0700

    ADD - file loginpage.dart

---
+++ lib/navigation/loginpage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  final TextEdi
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
