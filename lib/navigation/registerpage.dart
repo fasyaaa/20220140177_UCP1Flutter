4a35a7b4729817fd7a9b3d0ab97dc0599fb7e923
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-04-25 16:55:29 +0700

    ADD - Last commit from class

---
+++ lib/navigation/registerpage.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/navigation/loginpage.dart';
+
+class Registerpage extends StatefulWidget {
+  const Registerpage({super.key});
+
+  @override
+  State<Registerpage> createState() => _RegisterpageState();
+}
+
+class _RegisterpageState extends State<Registerpage> {
+  @override
+  final TextEditingController namaLengkapController = TextEditingController();
+  final TextEditingController nomerTelpController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('DAFTAR AKUN BARU'),
+              Text(''),
+              // Text('Nama Lengkap'),
+              TextFormField(
+                controller: namaLengkapController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+                  hintText: 'Nama Lengkap'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your full name';
+                  }
+                  return null;
+                },
+              ),
+              Text(''),
+              // Text("Email"),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+                  hintText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              Text(''),
+              // Text("Nomer Telephone"),
+              TextFormField(
+                controller: nomerTelpController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.add_call),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+                  hintText: 'No Hp'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your phone number';
+                  }
+                  return null;
+                },
+              ),
+              Text(''),
+              // Text("Password"),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+                  hintText: 'Password'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your Password';
+                  }
+                  return null;
+                },
+              ),
+              Text(''),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => Loginpage()),
+                  );
+                },
+                child: Text('Daftar'),
+              ),
+              Text(''),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: Text('Sudah memiliki akun ? Silahkan Login disini'),
+                // Text('Login disini!', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.green)) ,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
