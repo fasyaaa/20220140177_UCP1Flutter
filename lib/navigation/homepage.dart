2b44114864f8e4f33be006821e5b8f0959f6d79f
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-04-28 23:15:11 +0700

    ADD - Maint Content Area

--- lib/navigation/homepage.dart
+++ lib/navigation/homepage.dart
@@ -52,13 +52,144 @@ class Homepage extends StatelessWidget {
                       Navigator.pushAndRemoveUntil(
                         context,
                         MaterialPageRoute(builder: (context) => Loginpage()),
-                        (route)=> false,
+                        (route) => false,
                       );
                     },
                     icon: Icon(Icons.logout),
                     color: Colors.white,
                   ),
                 ],
+              ),
+            ),
+            SizedBox(height: 12),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(color: Colors.white),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 150,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/ads.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        IntrinsicHeight(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: Container(
+                                    padding: EdgeInsets.all(50),
+                                    decoration: BoxDecoration(
+                                      color: Color.fromARGB(255, 65, 187, 69),
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.add_home_work,
+                                          color: Colors.white,
+                                          size: 50,
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          'Data Piket',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: Container(
+                                    padding: EdgeInsets.all(50),
+                                    decoration: BoxDecoration(
+                                      color: Color.fromARGB(255, 65, 187, 69),
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.group_add,
+                                          color: Colors.white,
+                                          size: 50,
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          'Data Pelanggan',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                          maxLines: 2,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.all(50),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 65, 187, 69),
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Column(
+                              children: [
+                                Icon(
+                                  Icons.receipt_long,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Barang Masuk / Keluar',
+                                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 15),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
