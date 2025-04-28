4b4f7e217960b9ef8da5c4cdfaf651324b3ef9b5
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-04-29 03:19:57 +0700

    ADD - Header detailpage

---
+++ lib/navigation/detailpage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/navigation/datapiketpage.dart';
+
+class Detailpage extends StatelessWidget {
+  final String tugasPiket;
+  final String tanggal;
+  final String email;
+  const Detailpage({
+    super.key,
+    required this.tugasPiket,
+    required this.tanggal,
+    required this.email,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 65, 187, 69),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Datapiketpage(email: email),
+                          ),
+                        );
+                      },
+                      icon: Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Detail $tugasPiket',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
