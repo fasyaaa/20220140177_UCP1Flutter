2c31ee913f70eda093a481532457239e55aa6ec4
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-04-29 03:49:13 +0700

    ADD - Display detail tugas piket

--- lib/navigation/detailpage.dart
+++ lib/navigation/detailpage.dart
@@ -51,6 +51,74 @@ class Detailpage extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(height: 12),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(color: Colors.white),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 40,
+                      horizontal: 20,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 5,
+                              child: Form(
+                                child: Text(
+                                  '$tanggal',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color.fromARGB(255, 65, 187, 69),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 8,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 65, 187, 69),
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              child: Text(
+                                '$email',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        Container(
+                          alignment: Alignment.center,
+                          width: double.infinity,
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 65, 187, 69),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Text(
+                            '$tugasPiket',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
